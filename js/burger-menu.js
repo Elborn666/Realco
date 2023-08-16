0b05111bf737571a07115d5e40a92dc42e09c557
Elborn666 <elborn18@gmail.com>
2023-08-16 17:30:16 +0300

    тест бургер меню

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,22 +1,14 @@
-const openBurgerBtn = document.querySelector('.menu__burger-icon')
-const closedBurgerBtn = document.querySelector('.menu__burger-closed')
-const menuBurger = document.querySelector('.menu__burger')
+const openBurgerBtn = document.querySelector('.header__burger-icon')
+const menuBurger = document.querySelector('.burger__menu')
 const body = document.querySelector('body')
-const eventsClose = document.querySelectorAll('.menu__burger-link')
+const eventsClose = document.querySelectorAll('.burger__link')
 
-openBurgerBtn.addEventListener('click', onOpenBurger)
-closedBurgerBtn.addEventListener('click', onClosedBurger)
+openBurgerBtn.addEventListener('click', toggleOpenBurger)
 
-function onOpenBurger (){
-    menuBurger.classList.add('active__burger')
-    body.classList.add('hidden')
+function toggleOpenBurger (){
+    menuBurger.classList.toggle('active__burger')
+    body.classList.toggle('hidden')
 }
-
-function onClosedBurger (){
-    menuBurger.classList.remove('active__burger')
-    body.classList.remove('hidden')
-}
-
 
 for (let eventClose of eventsClose) {
     eventClose.addEventListener('click', onCloseModalBurger);
