05cd19ddb2eeeb6a184ae6e0a3fc05b5be42de9b
Elborn666 <elborn18@gmail.com>
2023-08-16 22:57:51 +0300

    виправив бургер мню

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,13 +1,22 @@
-const openBurgerBtn = document.querySelector('.header__burger-icon')
+const openBurgerBtn = document.querySelector('.header__burger-open')
+const closeBurgerBtn = document.querySelector('.header__burger-close')
 const menuBurger = document.querySelector('.burger__menu')
 const body = document.querySelector('body')
 const eventsClose = document.querySelectorAll('.burger__link')
 
-openBurgerBtn.addEventListener('click', toggleOpenBurger)
+openBurgerBtn.addEventListener('click', onOpenBurger)
+closeBurgerBtn.addEventListener('click', onCloseBurger)
 
-function toggleOpenBurger (){
-    menuBurger.classList.toggle('active__burger')
-    body.classList.toggle('hidden')
+function onOpenBurger (){
+    menuBurger.classList.add('active__burger')
+    body.classList.add('hidden')
+    closeBurgerBtn.classList.add('active__burger')
+}
+
+function onCloseBurger (){
+    menuBurger.classList.remove('active__burger')
+    body.classList.remove('hidden')
+    closeBurgerBtn.classList.remove('active__burger')
 }
 
 for (let eventClose of eventsClose) {
