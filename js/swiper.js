81c488e3adffd4ad72f34d9a929e1408a9dcd1c3
Elborn666 <elborn18@gmail.com>
2023-08-18 10:45:07 +0300

    тест свайпера

--- js/swiper.js
+++ js/swiper.js
@@ -2,17 +2,23 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    speed: 1500,
+    effect: "flip",
 
     autoplay: {
-        delay: 3000,
+        delay: 2000,
         disableOnInteraction: false,
+    },
+    flip: {
+      limitRotation: true,
+      slideShadows: false,
     },
   
     // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-forward',
-      prevEl: '.swiper-button-back',
-    },
+    // navigation: {
+    //   nextEl: '.swiper-button-forward',
+    //   prevEl: '.swiper-button-back',
+    // },
 
     // // If we need pagination
     // pagination: {
